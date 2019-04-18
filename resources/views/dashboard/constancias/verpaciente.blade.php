9aae724351b21eaf838d6c7ea607d806021344fc
Moises Serrano <moycs777@gmail.com>
2019-04-17 22:01:29 -0400

    Modificando constancia

--- resources/views/dashboard/constancias/verpaciente.blade.php
+++ resources/views/dashboard/constancias/verpaciente.blade.php
@@ -44,28 +44,8 @@
                       style="width: 100%; height: 100% px; font-size: 13px; line-height: 18px; border: 4px solid #dddddd; padding: 10px;" 
                       id="detalle" required>
                     </textarea>
-
-              <form  method="POST" action="{{ route('pdf_generate_constancia') }}" >
-               
-                @csrf
-                                
-                <div class="form-group">
-                   <input type="hidden" name="id" class="form-control"
-                    value = "{{ $paciente->id }}"
-                  >
-                </div>
-
-                
-                <div class="row">
-                  
-                  <div class="form-group col-sm-6">
-                    <label for="sel1"><strong>Nombre</strong></label>
-                    <input type="text" name="first_name" class="form-control"
-                      value = "{{ $paciente->first_name }}" disabled="true"
-                    >  
-
                   </div>
-
+                              
                   <div class="form-group">
                     <center>
                       <div class="col-md-4 col-md-4-offset-8">
@@ -73,26 +53,7 @@
                       </div>
                     </center>
                   </div>
-   
-                </div>
 
-                <div class="form-group">
-                  <label for=""><strong>Detalle</strong></label>
-                  <textarea name="detalle" 
-                    placeholder="Describa detalle" rows = '15' cols= "80" required = "required" 
-                    style="width: 100%; height: 100% px; font-size: 13px; line-height: 18px; border: 4px solid #dddddd; padding: 10px;" 
-                    id="detalle" required>
-                  </textarea>
-                </div>
-
-                <div class="form-group">
-                  <center>
-                    
-                    <div class="col-md-4 col-md-4-offset-8">
-                      <button type="submit" class="btn btn-fill btn-primary">Salvar</button>
-                    </div>
-                  </center>
-                </div>
               </form>
             </div>
           </div>
@@ -102,4 +63,3 @@
   </div>
 </div>
 @stop
-
