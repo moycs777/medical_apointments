8bafa751a665daf6d12762ebcdeaa2d4f38b9655
Moises Serrano <moycs777@gmail.com>
2019-09-19 21:08:42 -0300

    login de admin

---
+++ resources/views/layouts/admin_login.blade.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+<head>
+    @include('partials.admin.head')
+    @yield('page-style')
+</head>
+<body class="">
+
+
+    @if(session('info'))
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-md-8-offset-2">
+             <div class="alert alert-success">
+                {{ session('info') }}
+             </div>
+          </div>
+        </div>
+      </div>
+    @endif
+
+    @if(count($errors))
+       <div class="container">
+         <div class="row">
+           <div class="col-md-8" col-md-offset-1>
+             <div class="alert alert-danger">
+               <ul>
+                 @foreach($errors->all() as $error)
+                   <li>{{ $error }}</li>
+                 @endforeach
+               </ul>
+             </div>
+           </div>
+         </div>
+       </div>
+    @endif
+
+    @yield('content')
+
+    @include('partials.admin.footer')
+    
+    @include('partials.admin.foot')
+
+    @yield('page-script')
+
+</div>
+</body>
+</html>
